20aadd1aa5a1fb55b2e824c0ec52543b3b8c9664
User <strickemasterice@gmail.com>
2026-07-20 10:48:54 +0200

    reweork

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.static(path.join(__dirname, '.')));
 
 // postgresql
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'ticketsystem',
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'postgres',
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 });
 
 // DB
